ad67011c68e017bfdef685ab0f34d7ede84ae0d2
Jorge Clon <Jorgeperez.alu@iespacomolla.com>
2019-11-26 16:08:54 +0100

    Añado proyecto y diseño inicial

---
+++ src/u4actividad2rcg/U4Actividad2RCG.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package u4actividad2rcg;
+
+/**
+ *
+ * @author Ricardo Candel
+ */
+public class U4Actividad2RCG {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
